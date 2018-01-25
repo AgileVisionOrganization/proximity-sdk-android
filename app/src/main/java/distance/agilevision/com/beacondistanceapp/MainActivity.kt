f4b27ce1a998364cdbd37929c0cda4affcf4f2a3
akoidan <akoidan@agilevision.ui>
2018-01-25 17:29:29 +0200

    Added distances

--- app/src/main/java/distance/agilevision/com/beacondistanceapp/MainActivity.kt
+++ app/src/main/java/distance/agilevision/com/beacondistanceapp/MainActivity.kt
@@ -7,13 +7,24 @@ import butterknife.BindView
 import butterknife.ButterKnife
 
 class MainActivity : AppCompatActivity(), OnScan, CoordinateTracker {
-    override fun onDistanceChange(distances: Map<Identifier, Double>) {
+    override fun onDistanceChange(i: Identifier, d: Double) {
+        mm[i]?.setText(d.toString())
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
+
+
+    val a = Identifier("00112233445566778899", "000000000000")
+    val c = Identifier("00112233445566778899", "111111111111")
+    val b = Identifier("00112233445566778899", "111111000000")
+
+    var mm :Map<Identifier, TextView> = mutableMapOf()
 
     @BindView(R.id.text_x) lateinit var tx: TextView
     @BindView(R.id.text_y) lateinit var ty: TextView
     @BindView(R.id.text_error) lateinit var error: TextView
+    @BindView(R.id.beacon_a) lateinit var ba: TextView
+    @BindView(R.id.beacon_b) lateinit var bb: TextView
+    @BindView(R.id.beacon_c) lateinit var bc: TextView
 
 
     override fun onCoordinateChange(x: Double, y: Double) {
@@ -35,15 +46,19 @@ class MainActivity : AppCompatActivity(), OnScan, CoordinateTracker {
         setContentView(R.layout.activity_main)
         ButterKnife.setDebug(BuildConfig.DEBUG)
         ButterKnife.bind(this)
+        mm += a to ba;
+        mm += b to bb;
+        mm += c to bc;
         tx.setText("detecting...")
         ty.setText("detecting...")
+
         val beacons: Map<Identifier, Point> = mapOf(
-                Identifier("00112233445566778899", "000000000000") to Point(0.0, 0.0),
-                Identifier("00112233445566778899", "111111111111") to Point(1.535, 5.66),
-                Identifier("00112233445566778899", "111111000000") to Point(0.0, 3.07)
+                a to Point(0.0, 0.0),
+                c to Point(1.535, 5.66),
+                b to Point(0.0, 3.07)
         )
-        var c = CoordinateCalculator(this, beacons)
-        val bleUtil = BleUtil(this, c)
+        var cc = CoordinateCalculator(this, beacons)
+        val bleUtil = BleUtil(this, cc)
         bleUtil.scanForDevices(this)
 
     }
